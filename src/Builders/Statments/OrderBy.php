e16327c5e1f38ba000f7dc7d2dfb23675f6574c6
wsudo <mahdiomati@gmail.com>
2023-01-08 01:10:43 +0330

    implement QueryBuilder::orderBy() method

--- src/Builders/Statments/OrderBy.php
+++ src/Builders/Statments/OrderBy.php
@@ -2,10 +2,34 @@
 
 namespace Wsudo\PhpQueryBuilder\Builders\Statments;
 
+use Wsudo\PhpQueryBuilder\Throwables\InvalidValueError;
+
 trait OrderBy
 {
-    public function orderBy(string|array $columns): self
+    public function orderBy(string|array $columns = ['id']): self
     {
+        if(is_string($columns))
+        {
+            $columns = trim($columns);
+            if(empty($columns))
+            {
+                throw new InvalidValueError("column name must be string and not empty , invalid passed to " . __METHOD__);
+            }
+            
+            if(!in_array($columns , $this->orders))
+            {
+                $this->orders[] = $columns; 
+            }
+        }
+
+        if(is_array($columns) && !empty($columns))
+        {
+            foreach($columns as $column)
+            {
+                $this->orderBy($column);
+            }
+        }
+
         return $this;
     }
     public function orderByDesc(string|array $columns): self
