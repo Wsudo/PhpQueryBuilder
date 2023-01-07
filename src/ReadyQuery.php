53e617df244a726823bbaf14aacd08e919420cbd
wsudo <mahdiomati@gmail.com>
2023-01-08 03:05:20 +0330

    add comments to ReadyQuery instance

--- src/ReadyQuery.php
+++ src/ReadyQuery.php
@@ -4,11 +4,28 @@ namespace Wsudo\PhpQueryBuilder;
 
 use Wsudo\PhpQueryBuilder\Interfaces\ReadyQueryInterface;
 
+/**
+ * A ReadyQuery Built By QueryBuilder
+ */
 class ReadyQuery implements ReadyQueryInterface
 {
+    /**
+     * query string built by QueryBuilder with param binding sanitization
+     * @var string
+     */
     public string $queryString = "";
+
+    /**
+     * list of query param bindings sanitized and built by QueryBuilder
+     * @var array
+     */
     public array $bindings = [];
 
+    /**
+     * setup new instance
+     * @param string $queryString
+     * @param array $bindings
+     */
     public function __construct(string $queryString = "", array $bindings = [])
     {
         if (is_string($queryString)) {
@@ -18,25 +35,51 @@ class ReadyQuery implements ReadyQueryInterface
             $this->bindings = $bindings;
         }
     }
+
+    /**
+     * get Query string built by QueryBuilder
+     * @return string
+     */
     public function getQueryString(): string
     {
         return $this->queryString;
     }
+
+    /**
+     * set QueryString which is sanitized for param bindings
+     * @param string $queryString
+     * @return ReadyQuery
+     */
     public function setQueryString(string $queryString): self
     {
         $this->queryString = $queryString;
         return $this;
     }
+
+    /**
+     * get list of param bindings sanitized by QueryBuilder
+     * @return array
+     */
     public function getBindings(): array
     {
         return $this->bindings;
     }
+
+    /**
+     * set list of param bindings sanitized for QueryString
+     * @param array $bindings
+     * @return ReadyQuery
+     */
     public function setBindings(array $bindings): self
     {
         $this->bindings = $bindings;
         return $this;
     }
 
+    /**
+     * check queryString has any param bindings or not
+     * @return bool
+     */
     public function hasBindings(): bool
     {
         return count($this->bindings) > 0;
