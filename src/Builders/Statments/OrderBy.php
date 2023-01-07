cc34a90936311cee22108b50103f640c55e6be2c
wsudo <mahdiomati@gmail.com>
2023-01-07 23:12:26 +0330

    add OrderBy trait methods

--- src/Builders/Statments/OrderBy.php
+++ src/Builders/Statments/OrderBy.php
@@ -4,5 +4,16 @@ namespace Wsudo\PhpQueryBuilder\Builders\Statments;
 
 trait OrderBy
 {
-    
+    public function orderBy(string|array $columns): self
+    {
+        return $this;
+    }
+    public function orderByDesc(string|array $columns): self
+    {
+        return $this;
+    }
+    public function orderByAsc(string|array $columns): self
+    {
+        return $this;
+    }
 }
