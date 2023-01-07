e323de7691c5bcaaae26d5957604eacd3bdcc4c1
wsudo <mahdiomati@gmail.com>
2023-01-08 01:03:22 +0330

    implement QueryBuilder::limit() method

--- src/Builders/Statments/Limit.php
+++ src/Builders/Statments/Limit.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace Wsudo\PhpQueryBuilder\Builders\Statments;
+use Wsudo\PhpQueryBuilder\Throwables\InvalidValueError;
 
 trait Limit
 {
     public function limit(int $number): self
     {
+        if($number < 1)
+        {
+            throw new InvalidValueError("limit number MUST be bigger then zero , invalid passed to " .__METHOD__);
+        }
+        $this->limit =$number;
         return $this;
     }
     public function offset(int $offset = 0): self
