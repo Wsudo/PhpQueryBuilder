43ef8bcf0ab9dd41b2701d515e54cf301f4ca997
wsudo <mahdiomati@gmail.com>
2023-01-08 02:29:45 +0330

    implement QueryBuilder::groupBy() method

--- src/Builders/Statments/GroupBy.php
+++ src/Builders/Statments/GroupBy.php
@@ -2,10 +2,32 @@
 
 namespace Wsudo\PhpQueryBuilder\Builders\Statments;
 
+use Wsudo\PhpQueryBuilder\Throwables\InvalidValueError;
+
 trait GroupBy
 {
     public function groupBy(string|array $columns): self
     {
+        $this->isSelect = true;
+
+        if(is_string($columns))
+        {
+            $columns = trim($columns);
+            if(empty($columns))
+            {
+                throw new InvalidValueError("column name must be string and not empty , invalid passed to " . __METHOD__);
+            }
+            $this->groups[] = $columns;
+        }
+
+        if(is_array($columns))
+        {
+            foreach($columns as $column)
+            {
+                $this->groupBy($column);
+            }
+        }
+        
         return $this;
     }
 }
