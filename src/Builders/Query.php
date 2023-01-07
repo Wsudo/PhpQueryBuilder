1f2c5de778ab4ff6a7bc0984cdbfa4d48dcc0cfa
wsudo <mahdiomati@gmail.com>
2023-01-07 23:14:34 +0330

    add Join statment trait methods

--- src/Builders/Query.php
+++ src/Builders/Query.php
@@ -73,38 +73,7 @@ class Query implements QueryBuilderInterface
     {
         return $this;
     }
-    public function innerJoin(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function leftJoin(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function rightJoin(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function crossJoin(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function innerJoinWhere(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function leftJoinWhere(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function rightJoinWhere(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
-    public function crossJoinWhere(string $table, $first, $operator, $second): self
-    {
-        return $this;
-    }
+    
 
     public function exportWhereStatments(): array
     {
--- src/Builders/Statments/Join.php
+++ src/Builders/Statments/Join.php
@@ -4,6 +4,37 @@ namespace Wsudo\PhpQueryBuilder\Builders\Statments;
 
 trait Join
 {
-    
+    public function innerJoin(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function leftJoin(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function rightJoin(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function crossJoin(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function innerJoinWhere(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function leftJoinWhere(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function rightJoinWhere(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
+    public function crossJoinWhere(string $table, $first, $operator, $second): self
+    {
+        return $this;
+    }
 }
 
