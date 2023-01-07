89732dd3736ea7d55e7dd4e5b6591656ece83302
wsudo <mahdiomati@gmail.com>
2023-01-07 21:44:33 +0330

    implement ReadyQuery

--- src/ReadyQuery.php
+++ src/ReadyQuery.php
@@ -2,8 +2,38 @@
 
 namespace Wsudo\PhpQueryBuilder;
 
-class ReadyQuery
-{
-    
-}
+use Wsudo\PhpQueryBuilder\Interfaces\ReadyQueryInterface;
 
+class ReadyQuery implements ReadyQueryInterface
+{
+    public string $queryString = "";
+    public array $bindings = [];
+
+    public function __construct(string $queryString = "", array $bindings = [])
+    {
+        if (is_string($queryString)) {
+            $this->queryString = $queryString;
+        }
+        if (is_array($bindings)) {
+            $this->bindings = $bindings;
+        }
+    }
+    public function getQueryString(): string
+    {
+        return $this->queryString;
+    }
+    public function setQueryString(string $queryString): self
+    {
+        $this->queryString = $queryString;
+        return $this;
+    }
+    public function getBindings(): array
+    {
+        return $this->bindings;
+    }
+    public function setBindings(array $bindings): self
+    {
+        $this->bindings = $bindings;
+        return $this;
+    }
+}
