ecf6f343beff21695477c632387b52d8af2b41b9
wsudo <mahdiomati@gmail.com>
2023-01-08 02:44:39 +0330

    implement QueryBuilder::havingFunc() method

--- src/Builders/Statments/Having.php
+++ src/Builders/Statments/Having.php
@@ -2,6 +2,8 @@
 
 namespace Wsudo\PhpQueryBuilder\Builders\Statments;
 
+use Wsudo\PhpQueryBuilder\Throwables\InvalidValueError;
+
 trait Having
 {
     public function havingFunc(string $functionName, array|string|int|float $arguments = [], string|array $operator, string|array $values = null): self
@@ -14,7 +16,14 @@ trait Having
             $operator = "=";
         }
 
+        if(empty($functionName) || str_contains($functionName , " "))
+        {
+            throw new InvalidValueError("sql function name must be valid ID name , string and not be empty , invalid passed to " . __METHOD__);
+        }
 
+        $type = empty($arguments) ? "functional_without_argument" : "functionoal";
+
+        $this->havings[] = ['type' => $type, 'function' => $functionName, 'arguments' => $arguments, 'operator' => $operator, 'values' => $values, 'bindings' => $values];
 
         return $this;
     }
