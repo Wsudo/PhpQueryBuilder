e0448e9893307cb3f5759e0cc4178757e839e54e
wsudo <mahdiomati@gmail.com>
2023-01-08 15:43:33 +0330

    add comments to props

--- src/Query.php
+++ src/Query.php
@@ -4,8 +4,31 @@ namespace Wsudo\PhpQueryBuilder;
 
 final class Query
 {
-    private static $storeQueriesEnabled = false;
+    /**
+     * set enable storing queries or not
+     * @var bool
+     */
+    private static bool $storeQueriesEnabled = false;
+
+    /**
+     * maximum number of Queries to store
+     * 
+     * NOTE: it will pop the oldest Query from storedQueries
+     * @var int
+     */
     private static int $maxStoredQueries = 10;
+
+    /**
+     * stored Queries for debug or something like that
+     * 
+     * NOTE: it will store multiple type of Queries like (WhereOnlyQuery , fullQueryBuilder , etc)
+     * @var array
+     */
     private static array $storedQueries = [];
+
+    /**
+     * storage of tagged Queries
+     * @var array
+     */
     private static array $taggedQueries = [];
 }
