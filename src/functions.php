8ab5931380bc776e8e68bdbb7e31884762c44d2d
wsudo <mahdiomati@gmail.com>
2023-01-08 15:05:10 +0330

    add custom async function

--- src/functions.php
+++ src/functions.php
@@ -7,19 +7,25 @@ namespace Wsudo\PhpQueryBuilder
     use Amp\Cancellation;
     use Amp\Future;
 
-    use function Amp\Future\awaitAll;
+    use function Amp\Future\awaitAll as ampAwaitAll;
+    use function Amp\async as ampAsync;
 
     function await(array|Future $futures , Cancellation $cancellation = null)
     {
         if(is_array($futures))
         {
-            return awaitAll($futures , $cancellation);
+            return ampAwaitAll($futures , $cancellation);
         }
 
         if($futures instanceof Future)
         {
             return $futures->await($cancellation);
         }
+    }
+
+    function async(\Closure $callable , ...$args)
+    {
+        return ampAsync($callable, ...$args);
     }
 }
 
