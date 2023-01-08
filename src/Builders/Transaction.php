d956cbe5005faa37d9cc5321345c2d290a4c135b
wsudo <mahdiomati@gmail.com>
2023-01-08 20:00:25 +0330

    add TransactionBuilder comments

--- src/Builders/Transaction.php
+++ src/Builders/Transaction.php
@@ -33,6 +33,10 @@ class Transaction implements TransactionInterface
      */
     protected TransactionType $transactionType;
 
+    /**
+     * create new Transaction Interface
+     * @param TransactionType|null $transactionType
+     */
     public function __construct(TransactionType $transactionType = null)
     {
         if(!is_null($transactionType))
@@ -41,49 +45,95 @@ class Transaction implements TransactionInterface
         }
     }
 
+    /**
+     * set transaction queries
+     * @param array $queries
+     * @return Transaction
+     */
     public function setQueries(array $queries):self
     {
         $this->queries = $queries;
         return $this;
     }
 
+    /**
+     * get transaction queries
+     * @return array
+     */
     public function getQueries():array
     {
         return $this->queries;
     }
 
+    /**
+     * set transaction is commited or not
+     * @param bool $commited
+     * @return Transaction
+     */
     public function setCommited(bool $commited):self
     {
         $this->commited = $commited;
         return $this;
     }
 
+    /**
+     * get transaction commited or not
+     * @return bool
+     */
     public function getCommited():bool
     {
         return $this->commited;
     }
 
+    /**
+     * set transaction type
+     * @param TransactionType $transactionType
+     * @return Transaction
+     */
     public function setTransactionType(TransactionType $transactionType):self
     {
         $this->transactionType = $transactionType;
         return $this;
     }
 
+    /**
+     * get transaction type
+     * @return TransactionType
+     */
     public function getTransactionType():TransactionType
     {
         return $this->transactionType;
     }
 
+    /**
+     * check has any Query or not
+     * @return bool
+     */
     public function hasQuery():bool
     {
         return count($this->queries) > 0;
     }
 
+    /**
+     * add Transaction Query
+     * 
+     * NOTE: those should one by one and those will execute top to down
+     * @param QueryBuilder $query
+     * @return Transaction
+     */
     public function query(QueryBuilder $query): self 
     {
         $this->queries[] = $query;
         return $this;
     }
+
+    /**
+     * set transaction as commited
+     * 
+     * NOTE: after commiting transaction you are not allow to add new Queries to transaction Queries !
+     * @param TransactionType $transactionType
+     * @return Transaction
+     */
     public function commit(TransactionType $transactionType) :self
     {
         $this->transactionType = $transactionType;
@@ -91,10 +141,25 @@ class Transaction implements TransactionInterface
         return $this;
     }
 
+    /**
+     * build transaction and return it as ReadyQuery
+     * 
+     * NOTE: this will done as blocking (non-async)
+     * @param DatabaseType $databaseType
+     * @return ReadyQuery
+     */
     public function build(DatabaseType $databaseType): ReadyQuery 
     {
         return (new ReadyQuery);
     }
+
+    /**
+     * build transaction and return it as ReadyQuery
+     * 
+     * NOTE: this will done as non-blocking (async)
+     * @param DatabaseType $databaseType
+     * @return ReadyQuery
+     */
     public function asyncBuild(DatabaseType $databaseType):Future 
     {
         return (new Future(new FutureState));
