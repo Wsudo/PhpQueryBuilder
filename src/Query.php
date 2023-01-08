0fd8cea47c29da2a6719a1a77c63e79cc7d9a9e9
wsudo <mahdiomati@gmail.com>
2023-01-08 15:38:30 +0330

    add maxStoreQueries prop which will use for pop and push QueryBuilder Instances into storedQueries

--- src/Query.php
+++ src/Query.php
@@ -5,6 +5,7 @@ namespace Wsudo\PhpQueryBuilder;
 final class Query
 {
     private static $storeQueriesEnabled = false;
+    private static int $maxStoredQueries = 10;
     private static array $Queries = [];
     private static array $taggedQueries = [];
 }
