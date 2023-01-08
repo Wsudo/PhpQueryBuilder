783d89ec1fb70d0aba235561dce58bb635478091
wsudo <mahdiomati@gmail.com>
2023-01-08 15:14:39 +0330

    add commets to functions.php

--- src/functions.php
+++ src/functions.php
@@ -13,6 +13,13 @@ namespace Wsudo\PhpQueryBuilder
     use function Amp\Future\awaitAll as ampAwaitAll;
     use function Amp\async as ampAsync;
 
+    /**
+     * await a future or list of futures to be execute
+     * @param array|Future $futures
+     * @param Cancellation|null $cancellation
+     * @throws Exception
+     * @return mixed
+     */
     function await(array|Future $futures , Cancellation $cancellation = null)
     {
         if(is_array($futures))
@@ -28,11 +35,23 @@ namespace Wsudo\PhpQueryBuilder
         throw new Exception("Future should pass to " . __FUNCTION__);
     }
 
+    /**
+     * make a new future from callable
+     * @param \Closure $callable
+     * @param array $args
+     * @return Future
+     */
     function async(\Closure $callable , ...$args):Future
     {
         return ampAsync($callable, ...$args);
     }
 
+    /**
+     * create new time out cancellation and return it out
+     * @param float $time time in seconds
+     * @param string $message message which be throw as TimeoutExceotion
+     * @return TimeoutCancellation
+     */
     function timeout(float $time , string $message = "operation timed out"):TimeoutCancellation
     {
         return new TimeoutCancellation( $time , $message);
