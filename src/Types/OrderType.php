6791e52ee7a52922af4766a06aa580136b30b023
wsudo <mahdiomati@gmail.com>
2023-01-10 17:39:39 +0330

    add toType method to OrderType

--- src/Types/OrderType.php
+++ src/Types/OrderType.php
@@ -18,5 +18,19 @@ enum OrderType
             default => throw new Exception("order type not supported")
         };
     }
+
+    public static function toType(string|OrderType $orderType)
+    {
+        if ($orderType instanceof OrderType) {
+            return $orderType;
+        }
+
+        return match (mb_strtolower($orderType)) {
+            "none" => OrderType::NONE,
+            "desc" => OrderType::DESC,
+            "asc" => OrderType::ASC,
+            default => throw new Exception("order type not supported"),
+        };
+    }
 }
 
