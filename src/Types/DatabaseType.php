0de9917cfa7c9a01c5ab3072e134720ed37447ab
wsudo <mahdiomati@gmail.com>
2023-01-10 17:17:51 +0330

    add toType method to DatabaseType

--- src/Types/DatabaseType.php
+++ src/Types/DatabaseType.php
@@ -2,8 +2,7 @@
 
 namespace Wsudo\PhpQueryBuilder\Types;
 
-use Exception;
-use Wsudo\PhpQueryBuilder\Throwables\DatabaseTypeNotSupportedException as ThrowablesDatabaseTypeNotSupportedException;
+use Wsudo\PhpQueryBuilder\Throwables\DatabaseTypeNotSupportedException;
 
 enum DatabaseType
 {
@@ -15,12 +14,26 @@ enum DatabaseType
     public static function toString(DatabaseType $databaseType)
     {
         return match ($databaseType) {
-            DatabaseType::MYSQL => "MYSQL" ,
-            DatabaseType::SQLITE => "SQLITE" ,
-            DatabaseType::POSTGRESQL => "POSTGRESQL" ,
-            DatabaseType::MSSQL => "MSSQL" ,
-            default => throw new ThrowablesDatabaseTypeNotSupportedException("database type not support")
+            DatabaseType::MYSQL => "MYSQL",
+            DatabaseType::SQLITE => "SQLITE",
+            DatabaseType::POSTGRESQL => "POSTGRESQL",
+            DatabaseType::MSSQL => "MSSQL",
+            default => throw new DatabaseTypeNotSupportedException("database type not support")
+        };
+    }
+
+    public static function toType(string|DatabaseType $databaseType)
+    {
+        if ($databaseType instanceof DatabaseType) {
+            return $databaseType;
+        }
+
+        return match (mb_strtolower($databaseType)) {
+            "mysql" => DatabaseType::MYSQL,
+            "sqlite" => DatabaseType::SQLITE,
+            "postgresql" => DatabaseType::POSTGRESQL,
+            "mssql" => DatabaseType::MSSQL,
+            default => throw new DatabaseTypeNotSupportedException("databaseType not supported")
         };
     }
 }
-
