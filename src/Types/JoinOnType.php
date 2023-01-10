ee1229625afb8c1a9d6d0063efb80a9727c312c2
wsudo <mahdiomati@gmail.com>
2023-01-10 17:21:59 +0330

    add toType method to JoinOnType

--- src/Types/JoinOnType.php
+++ src/Types/JoinOnType.php
@@ -17,5 +17,18 @@ enum JoinOnType
             default => throw new Exception("joinOnType not supported !") ,
         };
     }
+
+    public static function toType(string|JoinOnType $joinOnType)
+    {
+        if ($joinOnType instanceof JoinOnType) {
+            return $joinOnType;
+        }
+
+        return match (mb_strtolower($joinOnType)) {
+            "on" => JoinOnType::On,
+            "where" => JoinOnType::Where,
+            default => throw new Exception("joinOnType not supported !") ,
+        };
+    }
 }
 
