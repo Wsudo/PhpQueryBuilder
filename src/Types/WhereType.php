3aae2adac42ea0a0e8d560d8a39a39cbdd0ab30c
wsudo <mahdiomati@gmail.com>
2023-01-10 17:45:10 +0330

    add toType method to WhereType

--- src/Types/WhereType.php
+++ src/Types/WhereType.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Wsudo\PhpQueryBuilder\Types;
+
 use Wsudo\PhpQueryBuilder\Throwables\Exception;
 
 enum WhereType
@@ -19,17 +20,36 @@ enum WhereType
     public static function toString(WhereType $whereType)
     {
         return match ($whereType) {
-            WhereType::Basic => "Basic" ,
-            WhereType::Nested => "Nested" ,
-            WhereType::SubQuery => "SubQuery" ,
-            WhereType::In => "In" ,
-            WhereType::NotIn => "NotIn" ,
-            WhereType::Between => "Between" ,
-            WhereType::NotBetween => "NotBetween" ,
-            WhereType::IsNull => "IsNull" ,
-            WhereType::IsNotNull => "IsNotNull" ,
+            WhereType::Basic => "Basic",
+            WhereType::Nested => "Nested",
+            WhereType::SubQuery => "SubQuery",
+            WhereType::In => "In",
+            WhereType::NotIn => "NotIn",
+            WhereType::Between => "Between",
+            WhereType::NotBetween => "NotBetween",
+            WhereType::IsNull => "IsNull",
+            WhereType::IsNotNull => "IsNotNull",
             default => throw new Exception("whereType not supported")
         };
     }
-}
 
+    public static function toType(string|WhereType $whereType)
+    {
+        if ($whereType instanceof WhereType) {
+            return $whereType;
+        }
+
+        return match (mb_strtolower($whereType)) {
+            "basic" => WhereType::Basic,
+            "nested" => WhereType::Nested,
+            "subquery" => WhereType::SubQuery,
+            "in" => WhereType::In,
+            "notin" => WhereType::NotIn,
+            "between" => WhereType::Between,
+            "notbetween" => WhereType::NotBetween,
+            "isnull" => WhereType::IsNull,
+            "isnotnull" => WhereType::IsNotNull,
+            default => throw new Exception("whereType not supported"),
+        };
+    }
+}
